ecb9198ff45db34a868f5b716480498b16f00063
CashLee <cashlee96347@gmail.com>
2012-01-17 12:48:21 +0800

    [mod]Add iscroll in show.html

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//demo of iphone effect 
+$("#plus-button").click(function(){
+  $("#select-memu").css('background','white');
+});
+
+//change the page in js for logic
+function toShowList(){
+  window.location='show.html';
+}
